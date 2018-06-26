0f681f6994a06e95c066f3ae9f0a882831307320
Ivan Gaton <gaton@Ivans-MacBook-Pro.local>
2018-06-26 15:35:59 -0400

    updates

--- camera.js
+++ camera.js
@@ -8,16 +8,18 @@ const app = express();
 Create a server that responds to every request by taking a picture and piping it directly to the browser.
 *********************************************/
 
-// var av = require('tessel-av');
-// var os = require('os');
-// var http = require('http');
+var av = require('tessel-av');
+var os = require('os');
+var http = require('http');
 var port = 8000;
 var camera = new av.Camera();
 
-app.get((req, res) => {
-  res.send(200, { 'Content-Type': 'image/jpg' });
+app.get('/', (req, res) => {
+  res.writeHead(200, { 'Content-Type': 'image/jpg' });
 
   camera.capture().pipe(res);
 
-}).listen(port, () => console.log(`http://${os.hostname()}.local:${port}`));
+})
+
+app.listen(port, () => console.log(`http://${os.hostname()}.local:${port}`));
 
